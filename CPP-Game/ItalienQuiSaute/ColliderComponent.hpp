2e229c02622d7fa8ef20964b65ea10f93abf8f67
Titouan MATHIS <55627323+Lyrym@users.noreply.github.com>
2023-05-08 16:43:21 +0200

    ADD: collider Directions

--- CPP-Game/ItalienQuiSaute/ColliderComponent.hpp
+++ CPP-Game/ItalienQuiSaute/ColliderComponent.hpp
@@ -7,12 +7,17 @@
 class ColliderComponent : public Component
 {
 public:
+	std::string direction;
 	bool collided;
+
 	ColliderComponent(sf::Sprite* entitySprite)
 	{
 		this->entitySprite = entitySprite;
 		Tag = "COLLIDER";
+
+		direction = "ALL";
 		collided = false;
+		
 	}
 
 	bool Collision(sf::Sprite* otherSprite)
@@ -21,14 +26,42 @@ public:
 		sf::FloatRect otherHitbox = otherSprite->getGlobalBounds();
 
 		if (entityHitbox.intersects(otherHitbox)) {
+
+
+			//Top Limit Diff
+			if (((entityHitbox.top + entityHitbox.height) - otherHitbox.top) <= 3) { //if diff is between 3 pixel
+				direction = "FLOOR";
+			}
+
+			//Bot Limit Diff
+			else if ((entityHitbox.top - (otherHitbox.top + otherHitbox.height)) >= -3) { //if diff is between 3 pixel
+				direction = "TOP";
+			}
+
+			//Left Limit Diff
+			
+			else if (((entityHitbox.left + entityHitbox.width) - otherHitbox.left) <= 3) { //if diff is between 3 pixel
+				direction = "SIDE";
+			}
+
+			//Right Limit Diff
+
+			else if ((entityHitbox.left - (otherHitbox.left+otherHitbox.width)) >= -3) { //if diff is between 3 pixel
+				direction = "SIDE";
+			}
+
+
+
+			
 			collided = true;
 			return true;
 		}
-		
+
+		direction = "ALL";
 		collided = false;
 		return false;
 	}
 private:
-	
+
 	sf::Sprite* entitySprite;
 };
